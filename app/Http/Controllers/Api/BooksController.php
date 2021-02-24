d1f05e7e021f753692d9b827b09c0b3348ebe3f2
DMB-220 <dmb2200@gmail.com>
2021-02-24 19:12:11 +0200

    Create books API, get all boooks, pagainate 25 per page. and show book

---
+++ app/Http/Controllers/Api/BooksController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Book;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Http\Resources\BooksResource;
+
+class BooksController extends Controller
+{
+    public function index()
+    {
+        $books = Book::approved()
+            ->with('authors', 'genres')
+            ->latest()
+            ->paginate(25);
+
+        return BooksResource::collection($books);
+    }
+
+    public function show(Book $book)
+    {
+        return new BooksResource($book);
+    }
+}
---
+++ app/Http/Resources/BooksResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BooksResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => $this->price,
+            'authors' => $this->ArrayToString($this->authors),
+            'genre' => $this->ArrayToString($this->genres),
+        ];
+        //return parent::toArray($request);
+    }
+}
